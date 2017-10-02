8fdbf6ab2dc16d670e87546eceb60bd35cbb6af2
root <root@ip-172-31-16-245.ap-south-1.compute.internal>
2017-10-02 09:06:55 +0000

    add clustring example

---
+++ clustring.php
@@ -0,0 +1,25 @@
+<?php
+    require 'vendor/autoload.php'
+    // a simple, single-process, horizontal scalable http server listening on 5 ports
+    
+    # Creating a event loop for our server
+    $loop = React\EventLoop\Factory::create();
+
+    for ($i=0;$i<5;++$i) {
+        # check for documentation of stream_socket_server http://php.net/manual/en/function.stream-socket-server.php
+        $server=stream_socket_server('tcp://127.0.0.1:'.(808+$i));
+        stream_set_blocking($server, 0);
+        # add server to event loop with a callback function
+        $loop->addReadStream($server, function ($server) use ($i) {
+            # start accepting request from port 808+$i
+            $conn=stream_socket_accept($server);
+            $len=strlen($i)+4;
+            $data = "HTTP/1.1 200 OK\r\nContent-Length: $len\r\n\r\nHi\n";
+            # Wwrite header of response 
+            fwrite($conn,$data);
+            echo "Served on port 800$i\n";
+        });
+    }
+    echo "Access your brand new HTTP server on 127.0.0.1:808x. Replace x with any number from 0-9\n";
+    # start event loop
+    $loop->run();
---
+++ http-server.php
@@ -0,0 +1,20 @@
+<?php
+  require 'vendor/autoload.php'
+  # Creating a event loop for our server
+  $loop   = React\EventLoop\Factory::create();
+  # Creating a server and passing event loop to that socket 
+  $socket = new React\Socket\Server($loop);
+  # Creating a http server and passing socket 
+  $http   = new React\Http\Server($socket);
+  # Define a request event on http with callback  
+  $http->on("request", function($request, $response) {
+      # Write header of response 
+      $response->writeHead(["Content-Type" => "text/plain"]);
+      # end that request
+      $response->end("Hello World");
+  });
+  # listen http server on PORT 8080 
+  $socket->listen(8080);
+  # start event loop
+  $loop->run();
+?>
